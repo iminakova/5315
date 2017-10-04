0afdea011df8e44091e856d623b31fed6ae458cb
Irina Minakova <irinaminakova@minakova.local>
2017-10-04 15:54:26 -0700

    hw1 commit

--- empdata.js
+++ empdata.js
@@ -1,123 +1,123 @@
 {
   "data": [
     {
-      "name": "Garrett Winters",
-      "designation": "Accountant",
+      "name": "Chandler Bing",
+      "designation": "Data Processor",
       "salary": "$170,750",
       "joining_date": "2011/07/25",
-      "office": "Tokyo",
+      "office": "New York",
       "extension": "8422"
     },
     {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
+      "name": "Monica Geller",
+      "designation": "Chef",
       "salary": "$372,000",
       "joining_date": "2012/12/02",
       "office": "New York",
       "extension": "4804"
     },
     {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
+      "name": "Joey Tribbiani",
+      "designation": "Actor",
       "salary": "$86,000",
       "joining_date": "2009/01/12",
-      "office": "San Francisco",
+      "office": "New York",
       "extension": "1562"
     },
     {
-      "name": "Airi Satou",
-      "designation": "Accountant",
+      "name": "Rachel Greene",
+      "designation": "Assistant Buyer",
       "salary": "$162,700",
       "joining_date": "2008/11/28",
-      "office": "Tokyo",
+      "office": "New York",
       "extension": "5407"
     },
     {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
+      "name": "Phoebe Buffay",
+      "designation": "Masseuse",
       "salary": "$106,450",
       "joining_date": "2011/12/12",
       "office": "New York",
       "extension": "8330"
     },
     {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
+      "name": "Ross Geller",
+      "designation": "Paleontologist",
       "salary": "$183,000",
       "joining_date": "2008/11/13",
-      "office": "Edinburgh",
+      "office": "New York",
       "extension": "6373"
     },
     {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
+      "name": "Michael Scott",
+      "designation": "Regional Manager",
       "salary": "$433,060",
       "joining_date": "2012/03/29",
-      "office": "Edinburgh",
+      "office": "Scranton",
       "extension": "6224"
     },
     {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
+      "name": "Dwight Schrute",
+      "designation": "Assistant TO THE Regional Manager",
       "salary": "$313,500",
       "joining_date": "2012/12/18",
-      "office": "London",
+      "office": "Scranton",
       "extension": "3597"
     },
     {
-      "name": "Jane Smith",
-      "designation": "Javascript Developer",
+      "name": "Jim Halpert",
+      "designation": "Salesman",
       "salary": "$205,500",
       "joining_date": "2009/09/15",
-      "office": "San Francisco",
+      "office": "Scranton",
       "extension": "2360"
     },
     {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
+      "name": "Pam Beesly",
+      "designation": "Receptionist",
       "salary": "$217,500",
       "joining_date": "2012/09/26",
-      "office": "Edinburgh",
+      "office": "Scranton",
       "extension": "2290"
     },
     {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
+      "name": "Toby Flenderson",
+      "designation": "HR (Evil)",
       "salary": "$137,500",
       "joining_date": "2012/08/06",
-      "office": "San Francisco",
+      "office": "Scranton",
       "extension": "9608"
     },
     {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
+      "name": "Meredith Palmer",
+      "designation": "Supplier Relations",
       "salary": "$327,900",
       "joining_date": "2010/10/14",
-      "office": "Tokyo",
+      "office": "Scranton",
       "extension": "6200"
     },
     {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
+      "name": "Jan Levinson",
+      "designation": "VP Northeast",
       "salary": "$103,600",
       "joining_date": "2008/12/13",
-      "office": "Edinburgh",
+      "office": "New York",
       "extension": "1667"
     },
     {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
+      "name": "Andy Bernard",
+      "designation": "Nard Dog",
       "salary": "$90,560",
       "joining_date": "2008/12/19",
-      "office": "London",
+      "office": "Stamford",
       "extension": "3814"
     },
     {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
+      "name": "David Wallace",
+      "designation": "CEO",
       "salary": "$342,000",
       "joining_date": "2013/03/03",
-      "office": "Edinburgh",
+      "office": "New York",
       "extension": "9497"
     }
   ]
